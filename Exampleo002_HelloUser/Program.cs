55709d13fff301c76175678251d2b35164df637b
pavelV <psvodnev@gmail.com>
2022-08-04 14:37:05 +0300

    Example 002 Hello User

---
+++ Exampleo002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите ваше имя");
+// console.ReadLine() при помощи нее происходит считывание данных, 
+string username = Console.ReadLine();
+// Console.WriteLine выводит текст ( приветствие ) на консоль друг под другом
+Console.WriteLine ("Привет, ");
+Console.WriteLine (username);
+// Console.Write выводит текст ( приветствие ) на консоль в одну строчку
